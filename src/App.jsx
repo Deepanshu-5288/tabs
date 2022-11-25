bb1c0ad93a2f5bdd9cc4432ef68b893d3e7bb3ba
Deeta-max <deepanshugupta5288@gmail.com>
2022-11-25 19:35:11 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import "./app.css";
+
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url =  'https://course-api.com/react-tabs-project';
+
+function App() {
+
+    const [loading, setLoading] = useState(true);
+    const [tabs, setTabs]= useState([]);
+    const [value, setValue] = useState(0);
+    const fetchData = async () =>{
+        try {
+            const response = await fetch(url);
+        const data = await response.json();
+        setTabs(data);
+        setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+        
+
+    }
+
+    useEffect(() =>{
+        fetchData();
+    }, []);
+
+
+    if(loading){
+        return (
+            <section className='section loading'>
+                <h1>Loading...</h1>
+            </section>
+        );
+    }
+    const { company, dates, duties, title } = tabs[value]
+  return (
+    <section className='section'>
+        <div className='title'>
+            <h2>experience</h2>
+            <div className='underline'></div>
+        </div>
+        <div className='jobs-center'>
+            <div className='btn-container'>
+            {tabs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+            </div>
+            <article className='job-info'>
+            <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+            </article>
+        </div>
+        <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  )
+}
+
+export default App
